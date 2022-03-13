1ee417adb4b84639dff12b1388286231a2b7de45
Anandhcl3 <anandhcl3@gmail.com>
2022-03-13 15:48:40 +0530

    Master: added root module

--- web-cluster-root-module/remotestate.tf
+++ web-cluster-root-module/remotestate.tf
@@ -1,7 +1,7 @@
 terraform {
   backend "s3" {
     bucket = "devops-terraform-state-bucket-2022"
-    key    = "web_cluster_with_GIHUBACTION"
+    key    = "web_cluster_with_GIHUBACTION-v1"
     dynamodb_table = "terraform-up-and-running-locks"
     encrypt = true
     region = "ap-south-1"
